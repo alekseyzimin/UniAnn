ae6526640dea607e6acd259b2257b6938ac0d961
Aleksey Zimin <aleksey.zimin@gmail.com>
2026-04-03 11:36:15 -0400

    added gffread and gffcompare

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 set -xe
 ROOT=$PWD && \
 mkdir -p $ROOT/bin && \
+cp gffcompare gffread $ROOT/bin && \
 (cd src && make && cp uniann $ROOT/bin) && \
 (cd scripts && cp *.pl *.sh $ROOT/bin && chmod 0755 $ROOT/bin/*.{pl,sh}) && \
 echo "All done" || echo "Installation failed"
