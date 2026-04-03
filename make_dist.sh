74fc085c85eeb7bc846139ee428ea8ae0bb551a4
Aleksey Zimin <aleksey.zimin@gmail.com>
2026-04-03 11:42:02 -0400

    added script to make distribution

---
+++ make_dist.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+VERSION=1.0.0
+echo "Making distribution" && \
+rm -rf uniann && \
+mkdir -p uniann && \
+cp -r install.sh gffcompare gffread src scripts uniann && \
+tar cvzf uniann.$VERSION.tgz uniann && \
+rm -rf uniann && \
+echo "Done"
