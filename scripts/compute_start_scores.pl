074581a0ebafd6ab18a15500b438cf232fa96277
Aleksey Zimin <aleksey.zimin@gmail.com>
2026-04-13 16:10:33 -0400

    rewrore compute tart scores

--- scripts/compute_start_scores.pl
+++ scripts/compute_start_scores.pl
@@ -1,140 +1,119 @@
-#!/usr/bin/perl
-use strict;
-use warnings;
-
-my ($fasta, $gff) = @ARGV;
-die "Usage: $0 genome.fasta annotation.gff\n" unless $gff;
-
-# -----------------------------
-# Load genome FASTA
-# -----------------------------
-my %seq;
-my $id;
-
-open FA, "<", $fasta or die $!;
-while (<FA>) {
-    chomp;
-    if (/^>(\S+)/) {
-        $id = $1;
-        $seq{$id} = "";
-    } else {
-        $seq{$id} .= uc($_);
+#!/usr/bin/env perl
+#
+my @narray=("A","C","G","T");
+#initialize code hashes
+$n=0;
+$n2=0;
+$n3=0;
+for($i=0;$i<4;$i++){
+  $code{$narray[$i]}=$n;
+  $n++;
+  for($j=0;$j<4;$j++){
+    $code2{"$narray[$i]$narray[$j]"}=$n2;
+    $n2++;
+    for($k=0;$k<4;$k++){
+      $code3{"$narray[$i]$narray[$j]$narray[$k]"}=$n3;
+      $n3++;
     }
-}
-close FA;
-
-# -----------------------------
-# Reverse complement
-# -----------------------------
-sub revcomp {
-    my $s = reverse($_[0]);
-    $s =~ tr/ACGTN/TGCAN/;
-    return $s;
+  }
 }
 
-# -----------------------------
-# Parse GFF: collect exons and CDS per transcript
-# -----------------------------
-my %tx;   # $tx{id}{chr,strand,exons=>[],cds=>[]}
-
-open GFF, "<", $gff or die $!;
-while (<GFF>) {
-    next if /^#/;
-    chomp;
-    my @f = split /\t/;
-    next unless @f >= 9;
-
-    my ($chr,$src,$type,$start,$end,$score,$strand,$phase,$attr) = @f;
-
-    my ($idtag) = $attr =~ /ID=([^;]+)/;
-    my ($parent) = $attr =~ /Parent=([^;]+)/;
-
-    # mRNA/transcript defines the container
-    if ($type eq "mRNA" || $type eq "transcript") {
-        $tx{$idtag}{chr}    = $chr;
-        $tx{$idtag}{strand} = $strand;
+my $fasta=$ARGV[0];
+my $gff=$ARGV[1];
+my $score_seq=$ARGV[2];
+my $seq="";
+my $start=-1;
+open (FILE,"gffread -W -w /dev/stdout -g $fasta $gff |");
+while($line=<FILE>){
+  chomp($line);
+  if($line =~/^>/){
+    if($start>-1){
+      push(@seqs,uc(substr($seq,$start-20,25))) if(length(substr($seq,$start-20,25))==25);
     }
-
-    # Exons
-    if ($type eq "exon" && $parent) {
-        push @{$tx{$parent}{exons}}, [$start, $end];
+    $seq="";
+    if($line=~/\sCDS=(\d+)-(\d+)\s/){
+      $start=$1;
+    }else{
+      $start=-1;
     }
-
-    # CDS
-    if ($type eq "CDS" && $parent) {
-        push @{$tx{$parent}{cds}}, [$start, $end];
-    }
+  }else{
+    $seq.=$line;
+  }
 }
-close GFF;
 
-# -----------------------------
-# Build transcript sequences
-# -----------------------------
-foreach my $tid (keys %tx) {
-
-    my $chr    = $tx{$tid}{chr};
-    my $strand = $tx{$tid}{strand};
-    my @exons  = sort { $a->[0] <=> $b->[0] } @{$tx{$tid}{exons}};
-
-    next unless @exons;
-
-    # Concatenate exon sequences
-    my $tseq = "";
-    for my $e (@exons) {
-        my ($s,$e2) = @$e;
-        $tseq .= substr($seq{$chr}, $s-1, $e2-$s+1);
-    }
-
-    # Reverse complement if needed
-    $tseq = revcomp($tseq) if $strand eq "-";
-
-    # -----------------------------
-    # Find translation start site
-    # -----------------------------
-    my @cds = sort { $a->[0] <=> $b->[0] } @{$tx{$tid}{cds}};
-    next unless @cds;
-
-    # First CDS genomic coordinate
-    my ($cds_start, $cds_end) =
-        ($strand eq "+") ? @{$cds[0]} : @{$cds[-1]};
-
-    # Convert genomic CDS start to transcript coordinate
-    my $tpos = 0;
-    my $cds_tpos;
-
-    for my $e (@exons) {
-        my ($s,$e2) = @$e;
-        my $elen = $e2 - $s + 1;
-
-        if ($strand eq "+") {
-            if ($cds_start >= $s && $cds_start <= $e2) {
-                $cds_tpos = $tpos + ($cds_start - $s);
-                last;
-            }
-        } else {
-            if ($cds_end >= $s && $cds_end <= $e2) {
-                $cds_tpos = $tpos + ($e2 - $cds_end);
-                last;
-            }
-        }
-
-        $tpos += $elen;
-    }
-
-    next unless defined $cds_tpos;
-
-    # -----------------------------
-    # Extract ±20 bp around start
-    # -----------------------------
-    my $win_start = $cds_tpos - 20;
-    my $win_end   = $cds_tpos + 20;
-
-    $win_start = 0 if $win_start < 0;
-    $win_end   = length($tseq)-1 if $win_end >= length($tseq);
-
-    my $subseq = substr($tseq, $win_start, $win_end - $win_start + 1);
-
-    print ">$tid|start=$cds_tpos|len=", length($subseq), "\n";
-    print "$subseq\n";
+my $start_length=25;
+foreach $start_seq (@seqs){
+ #print "Training: $start_seq\n";
+ for(my $i=0;$i<$start_length;$i++) {$start_pwm[$i][$code{substr($start_seq,$i,1)}]++ if(defined($code{substr($start_seq,$i,1)}));}
+ for(my $i=0;$i<($start_length-1);$i++) {$start2_pwm[$i][$code2{substr($start_seq,$i,2)}]++ if(defined($code2{substr($start_seq,$i,2)}));}
+ for(my $i=0;$i<($start_length-2);$i++) {$start3_pwm[$i][$code3{substr($start_seq,$i,3)}]++ if(defined($code3{substr($start_seq,$i,3)}));}
+ $w++;
 }
+
+my $score_floor_value=1e-10;
+for(my $i=0;$i<$start_length;$i++){
+  for(my $j=0;$j<4;$j++){
+    $start_pwm[$i][$j]=log($start_pwm[$i][$j]/$w*4+$score_floor_value);
+  }
+}
+
+for(my $i=0;$i<$start_length-1;$i++){
+  for(my $j=0;$j<16;$j++){
+    $start2_pwm[$i][$j]=log($start2_pwm[$i][$j]/$w*16+$score_floor_value);
+  }
+} 
+
+for(my $i=0;$i<$start_length-2;$i++){
+  for(my $j=0;$j<64;$j++){
+    $start3_pwm[$i][$j]=log($start3_pwm[$i][$j]/$w*64+$score_floor_value);
+  }
+} 
+
+#we load the genome sequences
+open(FILE,$score_seq);
+while(my $line=<FILE>){
+  chomp($line);
+  if($line=~ /^>/){
+    if(not($scf eq "")){
+      $genome_seqs{$scf}=$seq;
+      $seq="";
+    }
+    my @f=split(/\s+/,$line);
+    $scf=substr($f[0],1);
+  }else{
+    $seq.=$line;
+  } 
+}   
+$genome_seqs{$scf}=$seq if(not($scf eq ""));
+
+
+open(FILEST,">out.atg.txt");
+for my $g(keys %genome_seqs){
+  #only doing forward for now!!!
+  print "DEBUG scaffold $g $start_length $acceptor_length\n";
+  my $seq_fwd=uc($genome_seqs{$g});
+  my $seq_rev=$seq_fwd;
+  my @start_fwd_pos=();
+  #find starts fwd
+  while ($seq_fwd =~ /ATG/g) {
+    push @start_fwd_pos, pos($seq_fwd) - 3 if(pos($seq_fwd)>23);  # subtract length of "ATG" (2) to get start index
+  }
+  for $pos(@start_fwd_pos){
+    my $start_seq=substr($seq_fwd,$pos-19,$start_length);
+    my $start_hmm2_score=0;
+    my $start_hmm2_nscore=0;
+    
+    for(my $i=0;$i<($start_length-2);$i++){
+      $start_hmm2_score+=$start3_pwm[$i][$code3{substr($start_seq,$i,3)}] if(defined($code3{substr($start_seq,$i,3)}));
+    }
+    $start_hmm2_score+=$start2_pwm[0][$code2{substr($start_seq,0,2)}] if(defined($code2{substr($start_seq,0,2)}));
+    print "$pos $start_seq $start_score $start_hmm_score $start_hmm2_score\n";
+    $startfwd_hmm2_score{$pos}=$start_hmm2_score;
+  }
+}
+
+
+
+
+
 
