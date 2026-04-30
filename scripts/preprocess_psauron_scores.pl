3e7855295081bd17436cb85d6f565ccf31bc9556
Aleksey Zimin <aleksey.zimin@gmail.com>
2026-04-30 10:36:15 -0400

    more optimizations

--- scripts/preprocess_psauron_scores.pl
+++ scripts/preprocess_psauron_scores.pl
@@ -61,9 +61,13 @@ for my $g(keys %genome_seqs){
   my $j=0;
   #insert large negative score for an in frame stop 
   for(my $i=0;$i<length($seq_fwd)-3;$i+=3){
-    splice @psauron_frame0,$j,0,$stop_value if(is_stop(substr($seq_fwd,$i,3)));
-    splice @psauron_frame1,$j,0,$stop_value if(is_stop(substr($seq_fwd,$i+1,3)));
-    splice @psauron_frame2,$j,0,$stop_value if(is_stop(substr($seq_fwd,$i+2,3)));
+    if(is_stop(substr($seq_fwd,$i,3))){
+      splice @psauron_frame0,$j,0,$stop_value;
+    }elsif(is_stop(substr($seq_fwd,$i+1,3))){
+      splice @psauron_frame1,$j,0,$stop_value;
+    }elsif(is_stop(substr($seq_fwd,$i+2,3))){
+      splice @psauron_frame2,$j,0,$stop_value;
+    }
     $j++;
   }
 
